f6626af99843440819420252e225daba3ea38b25
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2023-12-25 20:02:04 +0700

    feat:finish all ex

--- ex-1.js
+++ ex-1.js
@@ -2,6 +2,28 @@
 
 const romanToInt = function (s) {
   //Start coding here
+  const romanNumber = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const currentNumber = romanNumber[s[i]];
+    const nextNumber = romanNumber[s[i + 1]];
+
+    if (nextNumber > currentNumber) {
+      result += nextNumber - currentNumber;
+      i++;
+    } else {
+      result += currentNumber;
+    }
+  }
+  return console.log(result);
 };
 
 const result1 = romanToInt("III"); // 3
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,22 @@
 
 const fizzBuzz = function (n) {
   //Start coding here
+  let arrayN = [];
+  for (let i = 0; i < n; i++) {
+    arrayN.push(i + 1);
+  }
+  const result = arrayN.map((item) => {
+    if (item % 3 === 0 && item % 5 === 0) {
+      return (item = "FizzBuzz");
+    } else if (item % 3 === 0) {
+      return (item = "Fizz");
+    } else if (item % 5 === 0) {
+      return (item = "Buzz");
+    } else {
+      return (item = String(item));
+    }
+  });
+  return result;
 };
 
 const result1 = fizzBuzz(3);
